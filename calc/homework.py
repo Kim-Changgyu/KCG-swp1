08fe13402cf22946f5ff2eafc208e35fa833b9ad
Kim-Changgyu <kimc980106@naver.com>
2020-06-21 17:54:09 +0900

    Issue #6 Add Try-Except statments for value error

--- calc/homework.py
+++ calc/homework.py
@@ -6,11 +6,11 @@ def application(environ, start_response):
 	a = d.get("a", [""])[0]
 	b = d.get("b", [""])[0]
 	response_body = html
-	if "" in [a, b]:
-		response_body = html.replace("Message", "Please input the value.")
-	else:
-		a, b = int(a), int(b)
-		response_body = html.replace("Message", "SUM(a+b) : {}, PRODUCT(a*b) : {}".format(a+b, a*b))
+	try:
+		a,b = int(a), int(b)
+		response_body = html.replace("Message", "SUM(a+b) : {}. MUL(a*b) : {}".format(a+b, a*b))
+	except ValueError:
+		response_body = html.replace("Message", "Please input correct type of value.") 
 	start_response("200 OK", [("Content-Type", "text/html"), ("Content-Length", str(len(response_body)))])
 	return [response_body]
 		
