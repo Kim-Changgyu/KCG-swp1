9b9b3b5f58371caf2089d607e2bc30a7bf5613bf
Kim-Changgyu <kimc980106@naver.com>
2020-06-21 16:56:07 +0900

    Issue #5 Fixed an error when no value was entered. (Including first visit case)

--- calc/homework.py
+++ calc/homework.py
@@ -6,9 +6,11 @@ def application(environ, start_response):
 	a = d.get("a", [""])[0]
 	b = d.get("b", [""])[0]
 	response_body = html
-	if "" not in [a, b]:
+	if "" in [a, b]:
+		response_body = html.replace("Message", "Please input the value.")
+	else:
 		a, b = int(a), int(b)
-		response_body = html.replace("Click the button.", "SUM(a+b) : {}, PRODUCT(a*b) : {}".format(a+b, a*b))
+		response_body = html.replace("Message", "SUM(a+b) : {}, PRODUCT(a*b) : {}".format(a+b, a*b))
 	start_response("200 OK", [("Content-Type", "text/html"), ("Content-Length", str(len(response_body)))])
 	return [response_body]
 		
